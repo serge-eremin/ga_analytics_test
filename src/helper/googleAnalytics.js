da0f8d98dc04e2f29cab7b977faf691aa1384bce
Sergey <velesinc@gmail.com>
2024-02-17 11:09:28 -0500

    added streams

--- src/helper/googleAnalytics.js
+++ src/helper/googleAnalytics.js
@@ -1,6 +1,25 @@
 export function InitializeReactGA(ReactGA) {
   if (!window.GA_INITIALIZED) {
-    ReactGA.initialize('G-90HR2WVVJM')
+      ReactGA.initialize(
+          [
+            {
+              trackingId: 'G-2VCQLP8SWM',
+              gaOptions: {
+                name: 'homepage_tracker',
+              },
+              // gtagOptions: {...}
+            },
+            {
+              trackingId: 'G-01QY50FKYF',
+              gaOptions: { name: 'blogpage_tracker' }
+            },
+            {
+              trackingId: 'G-RSP3XV0E0G',
+              gaOptions: { name: 'aboutpage_tracker' }
+            }
+          ],
+          { testMode: true }
+    )
     window.GA_INITIALIZED = true
   }
 }
