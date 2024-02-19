cbda327b3258256c0df615f5eec9dba1779a7a68
Sergey <velesinc@gmail.com>
2024-02-19 14:55:43 -0500

    refactored code 5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 
 import { Homepage } from './pages/Homepage'
